1f49c2ce589923bc5a904bd03c5a482d1361fd17
Camila Souza <cachaves22@gmail.com>
2021-05-04 14:31:08 -0700

    Factor into module, add integer palindrome detector

--- lib/casouza_video_palindrome.rb
+++ lib/casouza_video_palindrome.rb
@@ -1,16 +1,29 @@
 require_relative "casouza_video_palindrome/version"
 
-  class String
-
-    # Returns true for a palindrome, false otherwise.
-    def palindrome?
+module CasouzaVideoPalindrome
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    if processed_content.empty?
+      false
+    else
       processed_content == processed_content.reverse
     end
+  end
 
-    private
-  
-      # Returns content for palindrome testing.
-      def processed_content
-        scan(/[a-z]/i).join.downcase
-      end
+private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    to_s.scan(/[a-z\d]/i).join.downcase
+  end
+end
+
+  class String
+    include CasouzaVideoPalindrome
+    
+  end
+
+  class Integer
+    include CasouzaVideoPalindrome
+    
   end
