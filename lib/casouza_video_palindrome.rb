273b0a79c9511f0f7d1231d93910eb59cc04f947
Camila Souza <cachaves22@gmail.com>
2021-05-04 12:44:48 -0700

    Initialize repository

---
+++ lib/casouza_video_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "casouza_video_palindrome/version"
+
+module CasouzaVideoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
